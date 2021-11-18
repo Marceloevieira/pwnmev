5759df535d5aff4f0db2a85ed2facabb50e96d02
Marcelo E. Vieira <m.evangelista.vieira@gmail.com>
2021-11-18 00:09:37 -0400

    exercicio 2

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const http = require('http')
 const querystring = require('querystring');
 
-const html = function (pName = '',pSobrenome = '', pidade = '',pEndereco = '') {
+const form = () => {
 
     return `<form method="POST"  action="?" >
                 <fieldset>
                     <legend>Cadastro Pessoal</legend>
                         <p>
                             <label for="name">Nome:</label>
-                            <input type="text" name="name" value="${pName}"  />
+                            <input type="text" name="name"   />
                         </p>
                         <p>
                             <label for="sobreNome">Sobrenome :</label>
-                            <input type="text" name="sobreNome" value="${pSobrenome}" />
+                            <input type="text" name="sobreNome"  />
                         </p>
                         <p>
                             <label for="idade">Idade:</label>
-                            <input type="number" name="idade" value="${pidade}" />
+                            <input type="number" name="idade"  />
                         </p>
                         <p>
                             <label for="endereco">Endereço:</label>
-                            <input type="text" name="endereco" value="${pEndereco}" />
+                            <input type="text" name="endereco"  />
                         </p>
                         <p>
                             <input type="submit" name="submit">
@@ -30,11 +30,35 @@ const html = function (pName = '',pSobrenome = '', pidade = '',pEndereco = '') {
 
 }
 
+const submittedData = (pformData) =>{
+    
+    return `<fieldset>
+                <legend>Dados Submetidos</legend>
+                    <p>
+                        <label for="pName">Nome: ${pformData.name} </label>
+                        
+                    </p>
+                    <p>
+                        <label for="pSobrenome">Sobrenome : ${pformData.sobreNome}</label>
+                
+                    </p>
+                    <p>
+                        <label for="pIdade">Idade: ${pformData.idade}</label>
+                        
+                    </p>
+                    <p>
+                        <label for="pEndereco">Endereço: ${pformData.endereco}</label>
+                        
+                    </p>
+
+            </fieldset>`
+}
+
 const server = http.createServer((request, response) => {
 
     if (request.method == 'GET') {
         response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-            .end(html())
+            .end(form())
     } else if (request.method == 'POST') {
 
         let formData = [];
@@ -46,11 +70,10 @@ const server = http.createServer((request, response) => {
         });
 
         request.on('end', () => {
-            console.log(formData)
+            
             response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-                .end(html(formData.name,formData.sobreNome,formData.idade,formData.endereco))
+                .end(form()+submittedData(formData))
         });
-
 
     }
 
